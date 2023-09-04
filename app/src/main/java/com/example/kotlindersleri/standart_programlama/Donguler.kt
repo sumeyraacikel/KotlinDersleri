e02bbe5a72dd1d994ff7bba51a698249c269eeef
sumeyracikel <smeyraacikel@gmail.com>
2023-09-04 20:43:43 +0300

    second lesson

--- app/src/main/java/com/example/kotlindersleri/standart_programlama/Donguler.kt
+++ app/src/main/java/com/example/kotlindersleri/standart_programlama/Donguler.kt
@@ -1,2 +1,37 @@
 package com.example.kotlindersleri.standart_programlama
 
+fun main() {
+    for (i in 1..3){
+        println("Döngü 1 : $i ")
+    }
+
+    //10 ile 20, 5 er artsın
+    for (a in 10..20 step 5){//step infix function
+        println("Döngü 2 : $a")
+    }
+    //20 ile 10, 5 er azalsın
+    for (b in 20 downTo 10 step 5 ){
+        println("Döngü 3 : $b")
+    }
+
+    //1,2,3
+    var sayac = 1
+
+    while (sayac < 4 ) {
+        println("Döngü 4 : $sayac")
+        sayac++//sayac+=1//sayac = sayac + 1
+    }
+
+    for (i in 1..5){
+        if (i == 3){
+            break
+        }
+        println("Döngü 5 : $i")
+    }
+    for (i in 1..5){
+        if (i == 3){
+            continue
+        }
+        println("Döngü 6 : $i")
+    }
+}
--- app/src/main/java/com/example/kotlindersleri/standart_programlama/IfKullanimi.kt
+++ app/src/main/java/com/example/kotlindersleri/standart_programlama/IfKullanimi.kt
@@ -1,2 +1,53 @@
 package com.example.kotlindersleri.standart_programlama
 
+fun main() {
+    val yas = 17
+    val isim = "Mehmet"
+
+    if (yas >= 18 ){
+        println("Reşitsiniz")
+    }else {
+        println("Reşit değilsiniz")
+    }
+
+    if (isim == "Ahmet"){
+        println("Merhaba Ahmet")
+    }else if (isim == "Mehmet"){
+        println("Merhaba Mehmet")
+    }else{
+        println("Tanınmayan kişi")
+    }
+
+    val ka = "admin"
+    val s = 123456
+
+    if (ka == "admin" && s == 123456) {
+        println("Hoşgeldiniz")
+    }else{
+        println("Hatalı giriş")
+    }
+
+    val sayi = 30
+    if(sayi == 10 || sayi == 20){
+        println("Sayı 10 veya 20 dir")
+    }else {
+        println("Sayı 10 veya 20 değildir")
+    }
+
+    //Switch - When
+
+    val gun = 3
+
+    when(gun){
+        1 -> println("Pazartesi")
+        2 -> println("Salı")
+        3 -> println("Çarşamba")
+        4 -> println("Perşembe")
+        5 -> println("Cuma")
+        6 -> println("Cumartesi")
+        7 -> println("Pazar")
+        else -> println("Böyle bir gün yok")
+    }
+
+
+}
